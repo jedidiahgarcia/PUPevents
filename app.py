5a6a6a1744ede37e74048b8dcddbc95fd778f9d3
redperiabras <redperiabras@gmail.com>
2017-03-16 06:30:31 +0800

    removing permanently yung db password

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ session = {}
 app = Flask(__name__)
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_PASSWORD'] = '12345'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'pupevents'
 
 
